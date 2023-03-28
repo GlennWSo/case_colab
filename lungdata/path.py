0e8729c5676ac202c923a2f1b8f74067fd38be0f
GlennWSo <sondellglenn@gmail.com>
2023-03-28 17:29:34 +0200

    added helpfull hints for pathing to assets

--- lungdata/path.py
+++ lungdata/path.py
@@ -1,6 +1,14 @@
 import os
 
-SAMPLES_PATH = os.environ["LUNG_SOUND_PATH"]
+sound_env = "LUNG_SOUND_PATH"
+try:
+    SAMPLES_PATH = os.environ[sound_env]
+except KeyError:
+    print(f"Warning: {sound_env} environmet varible is not set")
+    default_dir = "db"
+    print(f"using default location for .wav files: ./{default_dir}")
+    SAMPLES_PATH = os.path.join(os.getcwd(), "db")
+    print(f"absolute path: {SAMPLES_PATH}")
 
 META_PATH = os.environ.get("LUNG_META_PATH")
 if META_PATH is None:
